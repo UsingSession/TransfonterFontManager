66e11296be380303d0a92623a0fcd9b80a6c1e69
UsingSession <arturtkaczenko@gmail.com>
2021-01-27 22:25:02 +0200

    Changing position the elements. Class refactoring

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -3,6 +3,7 @@ import pathlib, os, shutil
 from PyQt5 import QtCore, QtGui, QtWidgets, QtCore
 from PyQt5.QtWidgets import QFileDialog
 from PyQt5.QtGui import QIcon
+from threading import Timer
 
 Form, _ = uic.loadUiType(str(pathlib.Path(__file__).parent.absolute()) + '/ui/' + "app.ui")
 
@@ -17,39 +18,44 @@ class TransfonterFontManager(QtWidgets.QMainWindow, Form):
   def openDirectory(self, e):
     self.directory = QFileDialog.getExistingDirectory(None, 'Select a folder:', '', QFileDialog.ShowDirsOnly)
 
+  def clearProgress(self):
+    self.progressBar.setValue(0)
+    self.centralwidget.setEnabled(True)
+    
   def run(self, e):
+    self.countFiles = len(os.listdir(self.directory))
+    self.renameFiles()
+    self.groupFiles()
+    self.progressBar.setValue(100)
+    self.centralwidget.setDisabled(True)
+    Timer(2.0, self.clearProgress).start()
+
+  def renameFiles(self):
     str_replace = self.input_substring.text()
     if str_replace:
       for count, filename in enumerate(os.listdir(self.directory)):
+        self.progressBar.setValue(round((count * 100) / self.countFiles))
         if not os.path.isdir(os.path.join(self.directory, filename)):
           names, file_extension = os.path.splitext(filename)
           dst = self.directory + '/' + names.replace(str_replace, '') + file_extension
           src = self.directory + '/' + names + file_extension
           os.rename(src, dst)
-          if self.checkBox.isChecked():
-            path = self.directory + '/' + names.replace(str_replace, '')
-            if not os.path.isdir(path):
-              os.mkdir(path)
-            shutil.move(dst, path + '/' + names.replace(str_replace, '') + file_extension)
-    elif self.checkBox.isChecked():
+  
+  def groupFiles(self):
+    if self.checkBox.isChecked():
       for count, filename in enumerate(os.listdir(self.directory)):
+        self.progressBar.setValue(round((count * 100) / self.countFiles))
         if not os.path.isdir(os.path.join(self.directory, filename)):
-          names, file_extension = os.path.splitext(filename)
-          dst = self.directory + '/' + names.replace(str_replace, '') + file_extension
-          if self.checkBox.isChecked():
-            path = self.directory + '/' + names.replace(str_replace, '')
+            names, file_extension = os.path.splitext(filename)
+            dst = self.directory + '/' + names + file_extension
+            path = self.directory + '/' + names
             if not os.path.isdir(path):
               os.mkdir(path)
-            shutil.move(dst, path + '/' + names.replace(str_replace, '') + file_extension)
+            shutil.move(dst, path + '/' + names + file_extension)
 
   def init_UI(self):
-    self.setWindowTitle('File Manager for file')
-    self.input_substring.setPlaceholderText('Enter the input string.')
-    self.input_substring.setText('subset-')
-    self.directory_button.setIcon(QIcon(str(pathlib.Path(__file__).parent.absolute()) + '/static/img/' + 'folder.png')) 
     self.directory_button.clicked.connect(self.openDirectory)
     self.run_button.clicked.connect(self.run)
-
 
 if __name__ == '__main__':
   import sys
