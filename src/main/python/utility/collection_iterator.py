354847c97c070fc94b3c230776e3bbf6a59836fc
UsingSession <arturtkaczenko@gmail.com>
2021-02-21 00:02:51 +0200

    Adding methods; Update the list with a new item

--- src/main/python/utility/collection_iterator.py
+++ src/main/python/utility/collection_iterator.py
@@ -1,23 +1,12 @@
-
-
 class CollectionIterator:
 
   def __init__(self, items):
     self.items      = items
     self.itemsLen   = len(items)
+    self.commands   = None
     self.callback   = None
     self.curentPos  = None
-
-  def setCommands(self, commands):
-    self.commands = commands
-    return self
-
-  def getCommands(self):
-    return self.commands
-
-  def setAlter(self, callback):
-    self._callback = callback
-    return self
+    self.mutationItem = None
 
   def map(self):
     for result in map(self.iteration, self.items):
@@ -25,7 +14,25 @@ class CollectionIterator:
 
   def iteration(self,item):
     for command in self.commands:
-      mutationItem = command.process(item)
+      mutationItem = command.process(item, self.contextConfig)
+      index = self.items.index(item)
+      self.items[index] = mutationItem
+      item = mutationItem
       if self._callback != None:
-        self._callback(command, mutationItem, self.items)
-      return mutationItem
+        self._callback(command,mutationItem, self.items)
+    return item
+
+  def getCommands(self):
+    return self.commands
+
+  def setContextConfig(self, contextConfig):
+    self.contextConfig = contextConfig
+    return self
+
+  def setCommands(self, commands):
+    self.commands = commands
+    return self
+
+  def setAlter(self, callback):
+    self._callback = callback
+    return self
