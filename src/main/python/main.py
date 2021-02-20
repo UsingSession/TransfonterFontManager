5658b30216e46b667db9ec03d2fd64d79382c6a0
UsingSession <arturtkaczenko@gmail.com>
2021-02-21 00:03:45 +0200

    Transfer of processing methods in commandClass

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,13 +1,13 @@
-from PyQt5 import uic, QtWidgets
-from PyQt5 import QtCore, QtGui, QtWidgets, QtCore
+import os, sys
+from PyQt5 import uic, QtWidgets, QtCore, QtGui, QtWidgets, QtCore
 from PyQt5.QtWidgets import QFileDialog
 from PyQt5.QtGui import QIcon
 from threading import Timer
 from fbs_runtime.application_context.PyQt5 import ApplicationContext
-import pathlib, os, shutil, sys
+# Project scripts
 from utility.collection_iterator import CollectionIterator
-
-from commands.copy_command import CopyCommand
+from commands.rename_command import RenameCommand
+from commands.group_command import GroupCommand
 
 appctxt = ApplicationContext()
 Form, _ = uic.loadUiType(appctxt.get_resource("app.ui"))
@@ -16,7 +16,6 @@ class TransfonterFontManager(QtWidgets.QMainWindow, Form):
 
   def __init__(self):
     super(TransfonterFontManager, self).__init__()
-    # Isolation of a class for processing
     self.iterator = CollectionIterator
     self.setupUi(self)
     self.init_UI()
@@ -30,42 +29,20 @@ class TransfonterFontManager(QtWidgets.QMainWindow, Form):
     self.centralwidget.setEnabled(True)
     
   def run(self, e):
-    self.textEdit.setText('')
+    self.processMessage.setText('')
     self.iterator(os.listdir(self.directory)).setCommands([
-      CopyCommand
-    ]).setAlter(self.viewAlter).map()
-    
-    self.renameFiles()
-    self.groupFiles()
+      RenameCommand,
+      GroupCommand,
+    ]).setContextConfig(self).setAlter(self.viewAlter).map()
+
     self.progressBar.setValue(100)
     self.centralwidget.setDisabled(True)
-    Timer(2.0, self.clearProgress).start()
+    Timer(1.3, self.clearProgress).start()
 
-  def renameFiles(self):
-    str_replace = self.input_substring.text()
-    if str_replace:
-      for count, filename in enumerate(os.listdir(self.directory)):
-        if not os.path.isdir(os.path.join(self.directory, filename)):
-          names, file_extension = os.path.splitext(filename)
-          dst = self.directory + '/' + names.replace(str_replace, '') + file_extension
-          src = self.directory + '/' + names + file_extension
-          os.rename(src, dst)
-  
-  def groupFiles(self):
-    if self.checkBox.isChecked():
-      for count, filename in enumerate(os.listdir(self.directory)):
-        if not os.path.isdir(os.path.join(self.directory, filename)):
-            names, file_extension = os.path.splitext(filename)
-            dst = self.directory + '/' + names + file_extension
-            path = self.directory + '/' + names
-            if not os.path.isdir(path):
-              os.mkdir(path)
-            shutil.move(dst, path + '/' + names + file_extension)
-
-  def viewAlter(self, command, mutationItem, items):
+  def viewAlter(self, command,mutationItem, items):
     self.progressBar.setValue(round((items.index(mutationItem) * 100) / len(items)))
-    isNewLine = '\n' if self.textEdit.toPlainText() else ''
-    self.textEdit.setText(self.textEdit.toPlainText() + isNewLine + command.getProcessName() + ' ' + str(mutationItem))
+    isNewLine = '\n' if self.processMessage.toPlainText() else ''
+    self.processMessage.setText(self.processMessage.toPlainText() + isNewLine + '-> ' +  command.getProcessName() + ' ' + str(mutationItem))
 
   def getIcon(self, name):
     return QIcon(appctxt.get_resource(name))
@@ -75,7 +52,7 @@ class TransfonterFontManager(QtWidgets.QMainWindow, Form):
     self.directory_button.setIcon(self.getIcon("folder.png"))
     self.directory_button.clicked.connect(self.openDirectory)
     self.run_button.clicked.connect(self.run)
-    self.textEdit.setReadOnly(True)
+    self.processMessage.setReadOnly(True)
 
 if __name__ == '__main__':
   window = TransfonterFontManager()
