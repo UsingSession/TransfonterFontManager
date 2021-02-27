0de6f78adac51deb622aa6c99b4272b6ba0b133b
UsingSession <arturtkaczenko@gmail.com>
2021-02-27 22:55:28 +0200

    Adding new verification for commands (issubclass, willDoProcess)

--- src/main/python/utility/collection_iterator.py
+++ src/main/python/utility/collection_iterator.py
@@ -1,11 +1,14 @@
+from commands.abstract_command import AbstractCommand
+from PyQt5.QtWidgets import QMessageBox
+
 class CollectionIterator:
 
   def __init__(self, items):
-    self.items      = items
-    self.itemsLen   = len(items)
-    self.commands   = None
-    self.callback   = None
-    self.curentPos  = None
+    self.items        = items
+    self.itemsLen     = len(items)
+    self.commands     = None
+    self.callback     = None
+    self.curentPos    = None
     self.mutationItem = None
 
   def map(self):
@@ -14,12 +17,20 @@ class CollectionIterator:
 
   def iteration(self,item):
     for command in self.commands:
-      mutationItem = command.process(item, self.contextConfig)
-      index = self.items.index(item)
-      self.items[index] = mutationItem
-      item = mutationItem
-      if self._callback != None:
-        self._callback(command,mutationItem, self.items)
+      if self.isValidCommand(command):
+        doProcess = 
+        if not doProcess:
+          return item
+        else:
+          mutationItem = command.process(item, self.contextConfig)
+          index = self.items.index(item)
+          self.items[index] = mutationItem
+          item = mutationItem
+          if self._callback != None:
+            self._callback(command,mutationItem, self.items)
+      else:
+        self.getInvalidMessage(command)
+        self.commands.remove(command)
     return item
 
   def getCommands(self):
@@ -29,10 +40,17 @@ class CollectionIterator:
     self.contextConfig = contextConfig
     return self
 
-  def setCommands(self, commands):
+  def setCommands(self, commands):    
     self.commands = commands
     return self
 
   def setAlter(self, callback):
     self._callback = callback
     return self
+
+  def isValidCommand(self, command):
+    return issubclass(command, AbstractCommand)
+
+  def getInvalidMessage(self, command):
+    message = "%s is not extends %s" % (str(command.__name__), str(AbstractCommand.__name__))
+    QMessageBox.about(self.contextConfig, "Commands extends error", message)
