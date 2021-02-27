f6a12bccc7b8c9303c8a35ef97e9743dea41834d
UsingSession <arturtkaczenko@gmail.com>
2021-02-27 22:52:48 +0200

    Adding the command for file extension validation

---
+++ src/main/python/commands/valid_file.py
@@ -0,0 +1,20 @@
+import os, shutil
+from commands.abstract_command import AbstractCommand
+
+class ValidFile(AbstractCommand):
+
+  def getProcessName():
+    return 'Check file: '
+
+  def process(item, contextConfig):
+    return item
+  
+  def willDoProcess(item, contextConfig):
+    avaible_extensions = ['.woff', '.woff2', '.ttf', '.svg']
+    if not os.path.isdir(os.path.join(contextConfig.directory, item)):
+      names, file_extension = os.path.splitext(item)
+      if file_extension in avaible_extensions:
+        return True
+      else:
+        return False
+    return False
